069c97ca271b97af97c79d45dba2ac7caa40b789
dijiubuzhan <1012987012@qq.com>
2017-09-09 14:37:18 +0800

    add actionBar

--- app/src/main/java/com/test/myapplication/MvvmActivity.java
+++ app/src/main/java/com/test/myapplication/MvvmActivity.java
@@ -2,7 +2,9 @@ package com.test.myapplication;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.test.myapplication.bean.User;
 import com.test.myapplication.databinding.ActivityMvvmBinding;
@@ -19,6 +21,20 @@ public class MvvmActivity extends AppCompatActivity {
         user.setUsername("Sunzxyong");
         user.setPassword("12345678");
         binding.setUser(user);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
